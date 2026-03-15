5678cfc08f8dc2ace27219f0b780e38c32a19f00
machinewrapped <machinewrapped@users.noreply.github.com>
2026-03-15 20:39:58 +0100

    Change cask to formula

---
+++ Formula/smartcopy.rb
@@ -0,0 +1,23 @@
+class Smartcopy < Formula
+  desc "Intelligently copies large directories via composable filters and transform pipelines"
+  homepage "https://github.com/machinewrapped/SmartCopy2026"
+  version "0.0.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/machinewrapped/SmartCopy2026/releases/download/v#{version}/SmartCopy-#{version}-osx-arm64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # macos-arm64
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/machinewrapped/SmartCopy2026/releases/download/v#{version}/SmartCopy-#{version}-linux-x64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000" # linux-x64
+    end
+  end
+
+  def install
+    bin.install "SmartCopy"
+  end
+end
